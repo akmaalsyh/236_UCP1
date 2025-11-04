c4c2727a4dfeedcd2eddce696e4291178f9084c7
akmaalsyh <taufansyah50@gmail.com>
2025-11-04 09:48:59 +0700

    menambahkan method put untuk memperbarui atau mengupdate film yg ada

--- index.js
+++ index.js
@@ -37,3 +37,20 @@ app.get('/film', async (req, res) => {
         res.send(err);
     }
 });
+
+app.put('/film/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: 'Film tidak ditemukan' });
+        }
+
+        await film.update(data);
+        res.send({ message: 'Film berhasil diupdate', film });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
